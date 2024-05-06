ac0760531adc5e075b811e0e0dfd88823b18cf39
chaosannals <chaosannals@outlook.com>
2024-05-06 14:10:54 +0800

    jwt.

--- mid/jwt.go
+++ mid/jwt.go
@@ -5,14 +5,33 @@ import (
 	"os"
 	"strings"
 
-	"github.com/golang-jwt/jwt"
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/labstack/echo/v4"
 )
+
+func MakeJwtToken(claims jwt.Claims) (string, error) {
+	key := os.Getenv("CJUNGO_JWT_KEY")
+	if len(key) <= 0 {
+		return "", fmt.Errorf("生成 TOKEN 失败，没有配置 CJUNGO_JWT_KEY")
+	}
+
+	k := []byte(key)
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(k)
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("Bearer %s", tokenString), nil
+}
 
 func NewJwtAuthMiddleware(onResult func(*jwt.Token) error) func(echo.HandlerFunc) echo.HandlerFunc {
 	key := os.Getenv("CJUNGO_JWT_KEY")
 	if len(key) <= 0 {
-		key = "1234567890ABCDEF"
+		return func(next echo.HandlerFunc) echo.HandlerFunc {
+			return func(ctx echo.Context) error {
+				return fmt.Errorf("解析 TOKEN 失败，没有配置 CJUNGO_JWT_KEY")
+			}
+		}
 	}
 
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
@@ -28,13 +47,13 @@ func NewJwtAuthMiddleware(onResult func(*jwt.Token) error) func(echo.HandlerFunc
 					return nil, fmt.Errorf("加密算法不匹配 %v", token.Header["alg"])
 				}
 				k := []byte(key)
-				return &k, nil
+				return k, nil
 			})
 			if err != nil {
-				return err
+				return fmt.Errorf("解析 Token 失败, %v, %s", err, tokenString)
 			}
 			if err := onResult(token); err != nil {
-				return err
+				return fmt.Errorf("自定义 Token 处理返回错误：%v", err)
 			}
 
 			return next(ctx)
