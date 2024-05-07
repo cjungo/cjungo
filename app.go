9f143b6d69c9c7a9fe31c9470bb98b77020cbe25
chaosannals <chaosannals@outlook.com>
2024-05-07 14:19:39 +0800

    router.

--- app.go
+++ app.go
@@ -1,11 +1,8 @@
 package cjungo
 
 import (
-	"fmt"
 	"net/http"
 
-	"github.com/labstack/echo/v4"
-	"github.com/rs/zerolog"
 	"go.uber.org/dig"
 )
 
@@ -30,25 +27,7 @@ func NewApplication(handle ApplicationInitHandle) (*Application, error) {
 	}
 
 	// 路由
-	if err := container.Provide(func(logger *zerolog.Logger) *echo.Echo {
-		router := echo.New()
-
-		// 使用自定义上下文
-		router.Use(ResetContext)
-
-		// 异常处理句柄
-		router.HTTPErrorHandler = func(err error, c echo.Context) {
-			ctx := c.(HttpContext)
-			code := http.StatusInternalServerError
-			if he, ok := err.(*echo.HTTPError); ok {
-				code = he.Code
-			}
-			tip := fmt.Sprintf("%d: %v", code, err)
-			logger.Info().Str("error", tip).Msg("error:")
-			ctx.RespBad(tip)
-		}
-		return router
-	}); err != nil {
+	if err := container.Provide(NewRouter); err != nil {
 		return nil, err
 	}
 
