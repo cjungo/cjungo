c8e39760fe20bd62dc889c300a873bb3977eaca1
chaosannals <chaosannals@outlook.com>
2024-07-09 11:53:23 +0800

    permit.

--- mid/permit.go
+++ mid/permit.go
@@ -48,35 +48,42 @@ func (manager *PermitManager[TP, TS]) GetProof(ctx cjungo.HttpContext) (PermitPr
 	return nil, false
 }
 
+func permit[TP Permission, TS any](proof PermitProof[TP, TS], permissions ...TP) bool {
+	intersect := pie.Intersect(proof.GetPermissions(), permissions)
+	return len(intersect) > 0
+}
+
+// 权限逻辑 OR ，如果要实现 AND ，连续使用 多个 Permit 中间件
 func (manager *PermitManager[TP, TS]) Permit(permissions ...TP) echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
 			ctx := c.(cjungo.HttpContext)
 			reqID := ctx.GetReqID()
 			manager.logger.Info().Any("reqID", reqID).Msg("[PermitManager]")
+
+			// 当多个 Permit 中间件合用时，此处获取其他 Permit 中间件初始化好的。
 			if pp, ok := manager.proofs.Load(reqID); ok {
-				ppps := pp.(PermitProof[TP, TS]).GetPermissions()
-				added, _ := pie.Diff(ppps, permissions)
-				if len(added) > 0 {
-					return ctx.RespBadF("缺少权限: %v", added)
+				if permit(pp.(PermitProof[TP, TS]), permissions...) {
+					return next(ctx)
 				}
-				return next(ctx)
+				return ctx.RespBadF("缺少权限: %v", permissions)
 			}
+
 			if pp, err := manager.handle(ctx); err != nil {
 				return err
 			} else {
+				// 当多个 Permit 中间件合用时，此处做初始化。
 				manager.proofs.Store(reqID, pp)
 				manager.logger.Info().Any("store", pp).Str("id", reqID).Msg("[PermitManager]")
 				defer func() {
 					manager.logger.Info().Any("delete", pp).Str("id", reqID).Msg("[PermitManager]")
 					manager.proofs.Delete(reqID)
 				}()
-				added, _ := pie.Diff(pp.GetPermissions(), permissions)
-				if len(added) > 0 {
-					return ctx.RespBadF("缺少权限: %v", added)
-				}
 
-				return next(ctx)
+				if permit(pp, permissions...) {
+					return next(ctx)
+				}
+				return ctx.RespBadF("缺少权限: %v", permissions)
 			}
 		}
 	}
